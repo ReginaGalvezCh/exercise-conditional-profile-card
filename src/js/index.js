ef02f4f672adb54a17256557c199d49cbb8736eb
ReginaGalvezCh <reginagalvezch@gmail.com>
2020-09-10 19:29:04 +0000

    adding bootstrap

--- src/js/index.js
+++ src/js/index.js
@@ -27,16 +27,19 @@ function render(variables = {}) {
   // here we ask the logical questions to make decisions on how to build the html
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
-  if (variables.includeCover == false) cover = "<div class='cover'></div>";
-  let yourName = `<div class=""> <h1>${variables.name}</h1> </div>`;
+  if (variables.includeCover == false) cover = `<div class='cover'></div>`;
+  let yourName = `<div class="right"> <h2>${variables.name}</h2> </div>`;
   if (variables.name == null)
-    yourName = "<div class=''><h1>Your Name</h1></div>";
+    yourName = `<div class="right"><h2>Your Name</h2></div>`;
+  let yourLastName = `<div class="left"> <h2>${variables.lastname}</h2> </div>`;
+  if (variables.lastname == null)
+    yourLastName = `<div class="left"><h2>Your Last Name</h2></div>`;
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>${yourName}</h1>
+          <div class="row wrap"><div class="col-6">${yourName}</div><div class="col-6">${yourLastName}</div></div>
           <h2>Web Developer</h2>
           <h3>Miami, USA</h3>
           <ul class="position-right">
